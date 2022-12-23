a94637a18411b72425cea0d03dee124463c06b2a
Selim Mouaffak <selim@tsf-500-wpa-7-223.epfl.ch>
2022-12-23 16:36:33 +0100

    Plot of skewed out_degree

--- helpers.py
+++ helpers.py
@@ -1,6 +1,40 @@
+import math
+
+import networkx as nx
+import nltk
 import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+import requests
 from numpy.random import default_rng
+from plotly.subplots import make_subplots
 from sentence_transformers.util import cos_sim
+
+# We define the constatns for the analysis
+OUT_DEGREE = "out_degree"
+CLOSENESS = "closeness"
+PAGERANK = "pagerank"
+BETWEENNESS = "betweenness"
+SEMANTIC = "sematic similarity"
+CENTRALITY_MEASURES = [OUT_DEGREE, CLOSENESS, PAGERANK, BETWEENNESS]
+
+K = 12
+
+
+# We define the data schema for the analysis
+class DataSchema:
+    """This class defines the data schema for the analysis
+    """
+
+    START = "start"
+    TARGET = "target"
+    PATH = "path"
+    VISITS = "visits"
+    DURATION = "durationInSec"
+    PATH_LENGTH = "path_length"
+    TIME_PER_ARTICLE = "time_per_article"
+    TYPE = "type"
+
 
 # for a given series of visited pages, this method removes the nodes that are not in the final path in O(n)
 # by folding the back characters ("<") with the page preceding it and removing them from the path
@@ -110,10 +144,291 @@ def reduce(s):
     return s[0:index1][::-1]
 
 
+# This function will compute similarities between all page hops and the target article using transformers cos_sim
+def compute_cosine_similarity(df_embeddings, list_of_articles, target_article):
+    df_path = df_embeddings[
+        df_embeddings.file_name.apply(
+            lambda x: x in list_of_articles + [target_article]
+        )
+    ]
+    target_vector = df_path[df_path.file_name == target_article]["embeddings"].iloc[0]
+    list_of_vectors = [
+        df_path[df_path.file_name == article]["embeddings"].iloc[0]
+        for article in list_of_articles
+    ]
+    return cos_sim(
+        np.array(list_of_vectors).astype(float), np.array(target_vector).astype(float)
+    ).numpy()
 
-# This function will compute similarities between all page hops and the target article using transformers cos_sim  
-def compute_cosine_similarity(df_embeddings,list_of_articles, target_article):
-    df_path = df_embeddings[df_embeddings.file_name.apply(lambda x : x in list_of_articles+[target_article])]
-    target_vector = df_path[df_path.file_name == target_article]['embeddings'].iloc[0]
-    list_of_vectors = [df_path[df_path.file_name == article]['embeddings'].iloc[0] for article in list_of_articles]
-    return cos_sim(np.array(list_of_vectors).astype(float),np.array(target_vector).astype(float)).numpy()
+
+# Function to fetch summary from Wikipedia API
+def get_summary(title, num_sentences=4):
+    # Fetch summary from Wikipedia API
+    params = {
+        "format": "json",
+        "action": "query",
+        "prop": "extracts",
+        "exintro": "",
+        "explaintext": "",
+        "titles": title,
+        "redirects": 1,
+        "formatversion": 2,
+    }
+    api_url = "https://en.wikipedia.org/w/api.php"
+    response = requests.get(api_url, params=params).json()
+    pages = response["query"]["pages"]
+    page = pages[0]
+    try:
+        summary = page["extract"]
+    except:
+        summary = "No summary available"
+        return summary
+
+    # Tokenize summary into sentences
+    sentences = nltk.sent_tokenize(summary)
+
+    # Select first num_sentences sentences
+    summary = " ".join(sentences[:num_sentences])
+
+    return summary
+
+
+# Find to which percent of times your time belongs to
+def get_percent(times, your_time):
+    # Sort times in ascending order
+    times.append(your_time)
+    sorted_times = sorted(times)
+    # Find index of your time in sorted array
+    index = sorted_times.index(your_time)
+    # Calculate percent of times that you belong to
+    percent = (index + 1) / len(times) * 100
+    return np.ceil(percent)
+
+
+# We define a function to get the k most played games
+def get_top_k_games(df_paths_finished, k):
+    df_top_k = (
+        df_paths_finished[[DataSchema.START, DataSchema.TARGET, DataSchema.PATH]]
+        .groupby([DataSchema.START, DataSchema.TARGET])
+        .count()
+        .sort_values(by=DataSchema.PATH, axis=0, ascending=False)[:k]
+    )
+    top_k_games = df_top_k.index
+    return top_k_games
+
+
+# Function that returns the average evolution of a measure for the top k most played games
+def get_values_plot(df_paths_finished, k, measure, df_nodes=None, df_embeddings=None):
+    top_k_games = get_top_k_games(df_paths_finished, k)
+    mapping = {}
+    for i, game in enumerate(top_k_games):
+        path_measure = []
+
+        df_games = df_paths_finished[
+            [DataSchema.START, DataSchema.TARGET, DataSchema.PATH]
+        ]
+        mask = (df_games[DataSchema.START] == game[0]) & (
+            df_games[DataSchema.TARGET] == game[1]
+        )
+        df_games = df_games[mask]
+        num_games = df_games.shape[0]
+
+        if measure in CENTRALITY_MEASURES:
+            path_measure = list(
+                df_games[DataSchema.PATH].apply(
+                    lambda l: [df_nodes.loc[elem][measure] for elem in l]
+                )
+            )
+        else:
+            paths = df_games[DataSchema.PATH].values
+            for path in paths:
+                list_of_articles = path[:-1]
+                target_article = path[-1]
+                path_measure.append(
+                    compute_cosine_similarity(
+                        df_embeddings, list_of_articles, target_article
+                    )
+                )
+
+        xs, ys, l, u = get_value_evolution(path_measure)
+        mapping[game] = (xs, ys, l, u, num_games)
+
+    return mapping
+
+
+# Function that plots the average evolution of a measure for the top k most played games
+def plot_average_evolution(values, measure):
+    num_rows = math.floor(math.sqrt(len(values)))
+    num_cols = len(values) // num_rows
+    num_cols = num_cols if len(values) % num_rows == 0 else num_cols + 1
+
+    fig = make_subplots(
+        rows=num_rows,
+        cols=num_cols,
+        subplot_titles=[
+            "<b>{} to {} <br> ({} games</b>)".format(key[0], key[1], value[4])
+            for key, value in values.items()
+        ],
+        horizontal_spacing=0.15,
+    )
+
+    for i, (key, value) in enumerate(values.items()):
+        row = i // num_cols + 1
+        col = i % num_cols + 1
+        fig.update_xaxes(
+            title_text="path proportion",
+            row=row,
+            col=col,
+            tickfont_size=9,
+            titlefont_size=10,
+        )
+        fig.update_yaxes(
+            title_text="{} measure".format(measure),
+            row=row,
+            col=col,
+            tickfont_size=9,
+            titlefont_size=10,
+            ticksuffix=" ",
+        )
+        fig.add_trace(
+            go.Scatter(x=value[0], y=value[1], mode="lines", showlegend=False),
+            row=row,
+            col=col,
+        )
+        fig.add_trace(
+            go.Scatter(
+                x=value[0], y=value[2], mode="none", fill="tonexty", showlegend=False
+            ),
+            row=row,
+            col=col,
+        )
+        fig.add_trace(
+            go.Scatter(
+                x=value[0], y=value[3], mode="none", fill="tonexty", showlegend=False
+            ),
+            row=row,
+            col=col,
+        )
+
+    fig.update_layout(
+        height=900,
+        width=1200,
+        title_text="<b> Evolution of {} measure on shortest path \b ".format(
+            str(measure)
+        ),
+        title_x=0.5,
+    )
+    fig.update_annotations(font_size=13)
+
+    fig.show()
+
+
+# Function that yields shortest path from start to end
+def get_shortest_path(G, start, end):
+    return nx.shortest_path(G, start, end)
+
+
+# Function that yields evolution of centrality measure over shortest path for top k most played games
+def get_centrality_shortest_path(df_paths_finished, G_articles, df_nodes, measure, k=K):
+    top_k_games = get_top_k_games(df_paths_finished, k)
+    names = []
+    for i in top_k_games:
+        names.append("{} to {}".format(i[0], i[1]))
+    shortest_paths = [
+        get_shortest_path(G_articles, game[0], game[1]) for game in top_k_games
+    ]
+    path_measure = [
+        [df_nodes.loc[elem][measure] for elem in path] for path in shortest_paths
+    ]
+    x = []
+    for measure in path_measure:
+        x.append(list(range(len(measure))))
+    return list(zip(names, x, path_measure))
+
+
+# Function that plots the evolution of centrality measure over shortest path for top k most played games
+def plot_evolution_shortest_path(values, measure):
+    num_rows = math.floor(math.sqrt(len(values)))
+    num_cols = len(values) // num_rows
+    num_cols = num_cols if len(values) % num_rows == 0 else num_cols + 1
+
+    fig = make_subplots(
+        rows=num_rows,
+        cols=num_cols,
+        subplot_titles=[value[0] for value in values],
+        horizontal_spacing=0.07,
+    )
+
+    for i, value in enumerate(values):
+        row = i // num_cols + 1
+        col = i % num_cols + 1
+        fig.update_xaxes(
+            title_text="hop number",
+            row=row,
+            col=col,
+            tickfont_size=9,
+            titlefont_size=10,
+        )
+        fig.update_yaxes(
+            title_text="{} measure".format(measure),
+            row=row,
+            col=col,
+            tickfont_size=9,
+            titlefont_size=10,
+            ticksuffix=" ",
+        )
+        fig.add_trace(
+            go.Scatter(x=value[1], y=value[2], mode="lines", showlegend=False),
+            row=row,
+            col=col,
+        )
+
+    fig.update_layout(
+        height=900,
+        width=1000,
+        title_text="<b> Evolution of {} measure on shortest path \b ".format(
+            str(measure)
+        ),
+        title_x=0.5,
+    )
+    fig.update_annotations(font_size=13)
+
+    fig.show()
+
+
+# Compute similarities from each node to the target node for the shortest path of the top K most played games
+def get_sim_shortest_path(k, G_articles, df_paths_finished, df_embeddings):
+    top_k_games = get_top_k_games(df_paths_finished, k)
+    names = []
+    for i in top_k_games:
+        names.append("{} to {}".format(i[0], i[1]))
+    top_k_games_paths = []
+    similarities = []
+    for pair in set(top_k_games):
+        top_k_games_paths.append(get_shortest_path(G_articles, pair[0], pair[1]))
+
+    for i in range(k):
+        list_of_articles = top_k_games_paths[i][:-1]
+        target_article = top_k_games_paths[i][-1]
+        similarities.append(
+            compute_cosine_similarity(df_embeddings, list_of_articles, target_article)
+        )
+    y = [l.tolist() for l in similarities]
+    y = [sum(s, []) for s in y]
+    x = [list(range(len(y))) for y in y]
+
+    return list(zip(names, x, y))
+
+
+# We define a function to find how many different players have played certain games
+def get_number_players(df_paths_finished, top_k_games):
+    top_start = [i[0] for i in top_k_games]
+    top_target = [i[1] for i in top_k_games]
+    top_paths = list(zip(top_start, top_target))
+
+    df_pruned = df_paths_finished.merge(
+        pd.DataFrame(top_paths, columns=[DataSchema.START, DataSchema.TARGET]),
+        how="inner",
+        on=[DataSchema.START, DataSchema.TARGET],
+    )
+    return df_pruned.hashedIpAddress.nunique()
